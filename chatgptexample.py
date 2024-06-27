76ead1171c74eecfc018108b8b93e07e9dc0a985
Thomas Kirk <kirk.thomas.d@gmail.com>
2024-06-27 00:27:17 -0400

    small cleanup and threading unnecessary

--- chatgptexample.py
+++ chatgptexample.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+import time
 from typing import List, Any, Dict
 
 import requests
@@ -9,13 +11,13 @@ from dateutil.relativedelta import relativedelta
 
 from chesscom_cache import GameCache
 
-
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger()
 
 
 def main():
+    start = time.time()
     url = f'https://api.chess.com/pub/player/{username}/games/archives'
 
     # Make a GET request to the API to get a list of archives for the user
@@ -27,7 +29,13 @@ def main():
     archive_urls = [url for url in archive_urls if
                     get_end_of_month(get_date_from_archive_url(url)) >= lookback_date.date()]
 
-    create_chart([get_games_json(archive_url) for archive_url in archive_urls])
+    if MULTI_THREADED:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            month_json_futures = [executor.submit(get_games_json, archive_url) for archive_url in archive_urls]
+            create_chart(
+                [future.result() for future in (concurrent.futures.as_completed(month_json_futures))], start)
+    else:
+        create_chart([get_games_json(archive_url) for archive_url in archive_urls], start)
 
 
 def get_end_of_month(date: datetime):
@@ -45,17 +53,20 @@ def is_current_month(date_1: date):
 def get_games_json(archive_url: str):
     db = GameCache("chesscom_cache.db")
     start_of_month = get_date_from_archive_url(archive_url)
-    response_json = db.get(username, start_of_month)
-    if response_json is not None and not is_current_month(start_of_month):
-        logger.info("Found game archive in db for {}".format(archive_url))
-    else:
-        logger.info("Requesting games archive at {}".format(archive_url))
-        response_json = requests.get(archive_url, headers={"User-Agent": "5tk18"}).json()
-        db.set(username, start_of_month, response_json)
+    if USE_DB and not is_current_month(start_of_month):
+        response_json = db.get(username, start_of_month)
+        if response_json:
+            logger.info("Found game archive in db for {}".format(archive_url))
+            return response_json
+    logger.info("Requesting games archive at {}".format(archive_url))
+    response_json = requests.get(archive_url, headers={"User-Agent": "5tk18"}).json()
+    db.set(username, start_of_month, response_json)
     return response_json
 
 
-def create_chart(games_jsons: List[Dict[str, Any]]):
+def create_chart(games_jsons: List[Dict[str, Any]], start: time):
+    logger.info(f'Took {time.time() - start} seconds to fetch data')
+
     # Make a GET request to the API to get the games from the last 3 months
     elo_history = []
     game_ids = []
@@ -66,7 +77,7 @@ def create_chart(games_jsons: List[Dict[str, Any]]):
                     game['end_time']).date() >= lookback_date.date():
                 white_elo = game['white']['rating']
                 black_elo = game['black']['rating']
-                if username == game['white']['username']:
+                if username.lower() == game['white']['username'].lower():
                     elo_history.append(white_elo)
                 else:
                     elo_history.append(black_elo)
@@ -88,7 +99,9 @@ def create_chart(games_jsons: List[Dict[str, Any]]):
 
 # case sensistive
 username = '5tk18'
-days = 500
+days = 100
+USE_DB = True
+MULTI_THREADED = False
 
 if __name__ == '__main__':
     main()
