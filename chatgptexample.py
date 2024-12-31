cd234d58824f8badb17b0b8d24b0d9a7d8d3134a
Thomas Kirk <kirk.thomas.d@gmail.com>
2024-12-31 00:34:06 -0500

    messy code to do some different analysis

--- chatgptexample.py
+++ chatgptexample.py
@@ -1,20 +1,81 @@
+import io
+
 import requests
 import matplotlib.pyplot as plt
 import logging
 import sys
 from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta
+import chess.pgn
 
 from chesscom_cache import GameCache
 
 
 def main():
-    create_chart()
+    some_generic_method()
+    # create_chart()
     # now = datetime.now().replace(month=2)
     # # print(get_end_of_month(now))
     # for i in range(45):
     #     print("now plus {} days is {}".format(i, now + timedelta(i)))
     #     print(get_end_of_month(now + timedelta(i)))
+
+def some_generic_method():
+    db = GameCache("chesscom_cache.db")
+
+    # case sensistive
+    username = 'printerpaper'
+    url = f'https://api.chess.com/pub/player/{username}/games/archives'
+
+    days = 7
+
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    logging.getLogger().setLevel(logging.INFO)
+    logger = logging.getLogger()
+
+    # Make a GET request to the API to get a list of archives for the user
+    response = requests.get(url, headers={"User-Agent": "5tk18"})
+
+    # Get the URL of the archive for 3 months ago
+    lookback_date = datetime.now() - timedelta(days=days)
+    end_of_month = get_end_of_month(lookback_date)
+    archive_urls = response.json()['archives']
+    archive_urls = [url for url in archive_urls if
+                    get_end_of_month(get_date_from_archive_url(url)) >= lookback_date.date()]
+
+    for archive_url in archive_urls:
+        start_of_month = get_date_from_archive_url(archive_url)
+        response_json = db.get(username, start_of_month)
+        if response_json is not None and not is_current_month(start_of_month):
+            logger.info("Found game archive in db for {}".format(archive_url))
+        else:
+            logger.info("Requesting games archive at {}".format(archive_url))
+            response_json = requests.get(archive_url, headers={"User-Agent": "5tk18"}).json()
+            db.set(username, start_of_month, response_json)
+        total_seconds = 0
+        total_games = 0
+        for game in response_json['games']:
+            # and game['time_class'] == 'blitz'
+            if game['rules'] == 'chess' and datetime.fromtimestamp(game['end_time']).date() >= lookback_date.date():
+                try:
+                    game_duration = get_game_duration(game['pgn'])
+                    total_seconds += game_duration
+                    total_games += 1
+                    print(f"Spent {game_duration} seconds playing this game")
+                except Exception as e:
+                    logger.error(e)
+                    print(e)
+                    print("tommy error: " + game['url'])
+        print(f"Spent a total of {total_seconds} seconds playing {total_games} games")
+
+
+
+def get_game_duration(pgn_str: str):
+    pgn = chess.pgn.read_game(io.StringIO(pgn_str))
+    t1 = datetime.strptime(pgn.headers.get('UTCDate') + pgn.headers.get('StartTime'), "%Y.%m.%d%H:%M:%S")
+    t2 = datetime.strptime(pgn.headers.get('EndDate') + pgn.headers.get('EndTime'), "%Y.%m.%d%H:%M:%S")
+    t_delta = t2 - t1
+    return t_delta.seconds
 
 
 def get_end_of_month(date: datetime):
